2bcbe05f215ef7223c1d8aa652a6a243faf5e95b
salembooresli <70517601+salembooresli@users.noreply.github.com>
2020-09-16 17:22:48 +0300

    Done!!

--- SPEED2.playground/Contents.swift
+++ SPEED2.playground/Contents.swift
@@ -8,11 +8,16 @@
 
 */
 var numbers = [1,2,3,4,5,6,7,8,9,10]
-
+var evevnumbers = [2,4,6,8,10]
+for number in numbers {
+    print(number)
+}
 
 
 /*:
 
 # BONUS🎁 قم بكتابه كود يقوم بطباعة الأرقام الزوجية فقط 
 */
-
+for evevnumber in evevnumbers  {
+    print(evevnumber)
+}
